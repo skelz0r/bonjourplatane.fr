508ba3463cdff2be990069063f689577c522e8c3
Loïc Delmaire <loic.delmaire@gmail.com>
2021-10-20 19:56:25 +0200

    Introduce HumanFirstName

---
+++ lib/human_first_name.rb
@@ -0,0 +1,33 @@
+require 'open-uri'
+require 'json'
+require 'digest'
+
+class HumanFirstName
+  attr_reader :date
+
+  def initialize(date)
+    @date = date
+  end
+
+  def get
+    api_body['results'][0]['name']['first']
+  end
+
+  private
+
+  def api_body
+    JSON.parse(api_call)
+  end
+
+  def api_call
+    URI.parse(api_url).open.read
+  end
+
+  def api_url
+    "https://randomuser.me/api/?gender=male&nat=fr&seed=#{seed}"
+  end
+
+  def seed
+    date.strftime('%Y%m%d')
+  end
+end
